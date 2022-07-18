c58966a26b72c7c5a99d89533084b1581ca10277
degun <metamorphine.virus@gmail.com>
2022-07-18 10:42:35 +0200

    changes in admin

--- admin/preview-templates/page.js
+++ admin/preview-templates/page.js
@@ -2,13 +2,12 @@ import htm from "https://unpkg.com/htm?module";
 
 const html = htm.bind(h);
 
-// Preview component for a Page
 const Page = createClass({
   render() {
     const entry = this.props.entry;
 
     return html`
-      <main>
+      <main class="center about">
         <h1>${entry.getIn(["data", "title"], null)}</h1>
 
         ${this.props.widgetFor("body")}
