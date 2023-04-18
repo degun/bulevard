dc8ffeec155ca841788fa476e61977aa248f0c46
= <=>
2023-04-18 12:04:08 +0200

    carousel static working

--- _data/i18n/index.js
+++ _data/i18n/index.js
@@ -7,6 +7,18 @@ module.exports = {
         'en': 'What\'s on?',
         'sq': 'Cfare po shfaqet tani?'
       },
+      casting: {
+        'en': 'Casting gossips lab',
+        'sq': 'Laboratori i kastit thashethemeve'
+      },
+      bulevard: {
+        'en': 'The Bulevard show',
+        'sq': 'Shfaqja e Bulevardit'
+      },
+      thashetheme: {
+        'en': 'Thashetheme performance',
+        'sq': 'Performanca "Thashetheme"'
+      },
       location: {
         'en': 'Location',
         'sq': 'Vendndodhja'
--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -18,3 +18,34 @@ window.addEventListener('load', () => {
     })
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  const carousels = document.querySelectorAll('.carousel');
+  carousels.forEach(function(carousel) {
+    const controlPrev = carousel.querySelector('.carousel-control-prev');
+    const controlNext = carousel.querySelector('.carousel-control-next');
+    const items = carousel.querySelectorAll('.carousel-item');
+
+    controlPrev.addEventListener('click', function() {
+      moveCarousel(-1);
+    });
+
+    controlNext.addEventListener('click', function() {
+      moveCarousel(1);
+    });
+
+    function moveCarousel(direction) {
+      let activeIndex = -1;
+      items.forEach(function(item, index) {
+        if (item.classList.contains('active')) {
+          activeIndex = index;
+          item.classList.remove('active');
+        }
+      });
+
+      activeIndex = (activeIndex + direction + items.length) % items.length;
+      items[activeIndex].classList.add('active');
+    }
+  });
+});
+
+
